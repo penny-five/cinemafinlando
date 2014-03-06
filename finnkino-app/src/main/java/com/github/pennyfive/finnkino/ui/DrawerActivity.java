68a30e230b173b9de3a246d2eb653092c0232dda
Joonas Lehtonen <lehton83@gmail.com>
2014-03-06 21:01:04 +0200

    Created base class DrawerActivity that provides basic drawer functionality to implementing activities

---
+++ finnkino-app/src/main/java/com/github/pennyfive/finnkino/ui/DrawerActivity.java
@@ -0,0 +1,89 @@
+package com.github.pennyfive.finnkino.ui;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.DrawerLayout.DrawerListener;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.github.pennyfive.finnkino.FinnkinoApplication.InjectUtils;
+import com.github.pennyfive.finnkino.R;
+
+/**
+ * Base class for Activities that use {@link android.support.v4.widget.DrawerLayout}.
+ */
+public abstract class DrawerActivity extends Activity implements DrawerListener {
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle drawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_list);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        InjectUtils.inject(this);
+        initializeDrawerToggle();
+    }
+
+    private void initializeDrawerToggle() {
+        drawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawer,
+                R.drawable.ic_drawer,
+                R.string.drawer_open,
+                R.string.drawer_close
+        );
+        drawer.setDrawerListener(this);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+    }
+
+    protected final void setContentFragment(Fragment fragment) {
+        getFragmentManager().beginTransaction().replace(R.id.content, fragment).commit();
+    }
+
+    protected final void setDrawerFragment(Fragment fragment) {
+        getFragmentManager().beginTransaction().replace(R.id.drawer_content, fragment).commit();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onDrawerSlide(View drawerView, float slideOffset) {
+        drawerToggle.onDrawerSlide(drawerView, slideOffset);
+    }
+
+    @Override
+    public void onDrawerOpened(View drawerView) {
+        drawerToggle.onDrawerOpened(drawerView);
+        getActionBar().setTitle(R.string.app_name);
+        invalidateOptionsMenu();
+    }
+
+    @Override
+    public void onDrawerClosed(View drawerView) {
+        drawerToggle.onDrawerClosed(drawerView);
+        getActionBar().setTitle(R.string.app_name);
+        invalidateOptionsMenu();
+    }
+
+    @Override
+    public void onDrawerStateChanged(int newState) {
+        drawerToggle.onDrawerStateChanged(newState);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- finnkino-app/src/main/java/com/github/pennyfive/finnkino/ui/EventListActivity.java
+++ finnkino-app/src/main/java/com/github/pennyfive/finnkino/ui/EventListActivity.java
@@ -16,77 +16,14 @@
 
 package com.github.pennyfive.finnkino.ui;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v4.widget.DrawerLayout.DrawerListener;
-import android.view.MenuItem;
-import android.view.View;
 
-import com.github.pennyfive.finnkino.FinnkinoApplication.InjectUtils;
-import com.github.pennyfive.finnkino.R;
-
-import butterknife.InjectView;
-
-public class EventListActivity extends Activity implements DrawerListener {
-    @InjectView(R.id.drawer_layout) DrawerLayout drawer;
-    private ActionBarDrawerToggle drawerToggle;
+public class EventListActivity extends DrawerActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_event_list);
-        InjectUtils.inject(this);
-
-        getFragmentManager().beginTransaction().replace(R.id.content, new EventListFragment()).commit();
-        getFragmentManager().beginTransaction().replace(R.id.drawer_content, new NavigationFragment()).commit();
-
-        initializeDrawerToggle();
-    }
-
-    private void initializeDrawerToggle() {
-        drawerToggle = new ActionBarDrawerToggle(this, drawer, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close);
-        drawer.setDrawerListener(this);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        getActionBar().setHomeButtonEnabled(true);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        drawerToggle.syncState();
-    }
-
-    @Override
-    public void onDrawerSlide(View drawerView, float slideOffset) {
-        drawerToggle.onDrawerSlide(drawerView, slideOffset);
-    }
-
-    @Override
-    public void onDrawerOpened(View drawerView) {
-        drawerToggle.onDrawerOpened(drawerView);
-        getActionBar().setTitle(R.string.app_name);
-        invalidateOptionsMenu();
-    }
-
-    @Override
-    public void onDrawerClosed(View drawerView) {
-        drawerToggle.onDrawerClosed(drawerView);
-        getActionBar().setTitle(R.string.app_name);
-        invalidateOptionsMenu();
-    }
-
-    @Override
-    public void onDrawerStateChanged(int newState) {
-        drawerToggle.onDrawerStateChanged(newState);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (drawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        setContentFragment(new EventListFragment());
+        setDrawerFragment(new NavigationFragment());
     }
 }
