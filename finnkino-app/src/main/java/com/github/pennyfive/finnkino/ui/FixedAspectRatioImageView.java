58d4b7073d66cb880b77a3ca825ae6c67035c5d9
Joonas Lehtonen <lehton83@gmail.com>
2014-04-14 16:30:53 +0300

    Upgraded LandscapeEventImageView into more generic FixedAspectRatioImageView that doesn't use hardcoded aspect ratio.

--- finnkino-app/src/main/java/com/github/pennyfive/finnkino/ui/FixedAspectRatioImageView.java
+++ finnkino-app/src/main/java/com/github/pennyfive/finnkino/ui/FixedAspectRatioImageView.java
@@ -17,34 +17,47 @@
 package com.github.pennyfive.finnkino.ui;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
-/**
- * For displaying event promo images in fixed aspect ratio.
- */
-public class LandscapeEventImageView extends ImageView {
-    /**
-     * Promo images are always returned in this aspect ratio.
-     */
-    private static final float ASPECT_RATIO = 2.68f;
+import com.github.pennyfive.finnkino.R;
 
-    public LandscapeEventImageView(Context context) {
+/**
+ * For displaying images in fixed aspect ratio.
+ */
+public class FixedAspectRatioImageView extends ImageView {
+    private float aspectRatio = 1;
+
+    public FixedAspectRatioImageView(Context context) {
         super(context);
     }
 
-    public LandscapeEventImageView(Context context, AttributeSet attrs) {
+    public FixedAspectRatioImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        readAspectRatioFromAttributes(attrs);
     }
 
-    public LandscapeEventImageView(Context context, AttributeSet attrs, int defStyle) {
+    public FixedAspectRatioImageView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        readAspectRatioFromAttributes(attrs);
+    }
+
+    private void readAspectRatioFromAttributes(AttributeSet attrs) {
+        TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.FixedAspectRatioImageView);
+        aspectRatio = ta.getFloat(R.styleable.FixedAspectRatioImageView_aspectRatio, 1f);
+        ta.recycle();
+    }
+
+    public void setAspectRatio(float aspectRatio) {
+        this.aspectRatio = aspectRatio;
+        requestLayout();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         int width = MeasureSpec.getSize(widthMeasureSpec);
-        heightMeasureSpec = MeasureSpec.makeMeasureSpec((int) (width / ASPECT_RATIO), MeasureSpec.EXACTLY);
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec((int) (width / aspectRatio), MeasureSpec.EXACTLY);
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 }
