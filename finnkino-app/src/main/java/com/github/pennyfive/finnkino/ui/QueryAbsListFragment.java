167367f087dd0eb890f7f90a45941d4163b1a5e5
Joonas Lehtonen <lehton83@gmail.com>
2014-04-19 20:43:46 +0300

    Added loading animation to QueryAbsListFragment that is shown while waiting for content.

--- finnkino-app/src/main/java/com/github/pennyfive/finnkino/ui/QueryAbsListFragment.java
+++ finnkino-app/src/main/java/com/github/pennyfive/finnkino/ui/QueryAbsListFragment.java
@@ -16,6 +16,8 @@
 
 package com.github.pennyfive.finnkino.ui;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -24,9 +26,11 @@ import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.FrameLayout;
 
 import com.github.pennyfive.finnkino.R;
 import com.github.pennyfive.finnkino.api.model.Container;
@@ -71,6 +75,7 @@ public abstract class QueryAbsListFragment<T, S extends Container<T>> extends Fr
     }
 
     private AbsListView absListView;
+    private ViewGroup loadingView;
     private Adapter adapter;
 
     @Override
@@ -78,13 +83,20 @@ public abstract class QueryAbsListFragment<T, S extends Container<T>> extends Fr
         absListView = createAbsListView();
         absListView.setId(R.id.list);
         absListView.setOnItemClickListener(this);
-        return absListView;
+
+        loadingView = (ViewGroup) inflater.inflate(R.layout.fragment_loading_layer, null);
+
+        FrameLayout rootLayout = new FrameLayout(getActivity());
+        rootLayout.addView(absListView, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        rootLayout.addView(loadingView, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        return rootLayout;
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getLoaderManager().initLoader(0, savedInstanceState, this);
+        animateLoadingView();
     }
 
     @Override
@@ -99,6 +111,23 @@ public abstract class QueryAbsListFragment<T, S extends Container<T>> extends Fr
         // TODO error handling for cases when data is null
         adapter = new Adapter(getActivity(), data.getItems());
         absListView.setAdapter(adapter);
+        animateOutLoadingView();
+    }
+
+    private void animateLoadingView() {
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(getActivity(), R.animator.loading_spinner_in);
+        set.setTarget(loadingView.findViewById(R.id.spinner));
+        set.start();
+    }
+
+    private void animateOutLoadingView() {
+        /* Combination of two animations: spinner shrinks while the layout itself fades out */
+        AnimatorSet set1 = (AnimatorSet) AnimatorInflater.loadAnimator(getActivity(), R.animator.loading_layer_out);
+        set1.setTarget(loadingView);
+        AnimatorSet set2 = (AnimatorSet) AnimatorInflater.loadAnimator(getActivity(), R.animator.loading_spinner_out);
+        set2.setTarget(loadingView.findViewById(R.id.spinner));
+        set1.playTogether(set2);
+        set1.start();
     }
 
     @Override
